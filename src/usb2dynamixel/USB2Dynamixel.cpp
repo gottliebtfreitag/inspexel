85903c4c6e1b9242eac4834443967e6df702b5ed
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2018-08-25 09:20:06 +0200

    trying to make USB2Dynamixel.h more compact

--- src/usb2dynamixel/USB2Dynamixel.cpp
+++ src/usb2dynamixel/USB2Dynamixel.cpp
@@ -14,29 +14,29 @@
 #define TERM_RED                        "\033[31m"
 #define TERM_RESET                      "\033[0m"
 
-namespace dynamixel
-{
+namespace dynamixel {
 
-struct USB2Dynamixel::Pimpl
-{
+struct USB2Dynamixel::Pimpl {
+	simplyfile::SerialPort mPort;
+	std::mutex             mMutex;
+
 	std::chrono::high_resolution_clock::time_point now() const {
 		return std::chrono::high_resolution_clock::now();
 	}
-	simplyfile::SerialPort port;
 
 	Pimpl(std::vector<std::string> const& deviceNames, int baudrate) {
 		for (auto const& dev : deviceNames) {
 			try {
 				simplyfile::SerialPort p{dev, baudrate};
 				if (p.valid()) {
-					port = std::move(p);
+					mPort = std::move(p);
 					break;
 				}
 			} catch (std::runtime_error const& err) {
 				std::cerr << err.what() << std::endl;
 			}
 		}
-		if (not port.valid()) {
+		if (not mPort.valid()) {
 			std::stringstream ss;
 			ss << "Cannot open any port for usb2dynamixel. Tried: ";
 			for (auto const& dev : deviceNames) {
@@ -46,9 +46,7 @@ struct USB2Dynamixel::Pimpl
 		}
 	}
 
-	void writePacket(dynamixel::parameter const& packet)
-	{
-		const uint8_t *data = packet.data();
+	void writePacket(Parameter const& packet) const {
 		uint bytesWritten = 0;
 		const size_t count = packet.size();
 //		std::cout << "write: " << std::hex << std::setfill('0');
@@ -57,213 +55,186 @@ struct USB2Dynamixel::Pimpl
 //		}
 //		std::cout << std::dec << std::endl;
 		do {
-			ssize_t w = ::write(port, data + bytesWritten, count - bytesWritten);
+			ssize_t w = ::write(mPort, packet.data() + bytesWritten, count - bytesWritten);
 
 			if (w < 0) {
-				throw std::runtime_error("write to the dyanmixel bus failed");
-			} else {
-				bytesWritten += w;
+				throw std::runtime_error(std::string{"write to the dyanmixel bus failed: "} + strerror(errno) + " (" + std::to_string(errno) + ")");
 			}
+			bytesWritten += w;
+
 		} while (bytesWritten < count);
 	}
 
-	bool readPacket(parameter &i_packet, uint8_t incommingLength, timeout_t timeout)
-	{
+	void flushRead() const {
+		uint8_t dummy;
+		ssize_t r = 1;
+		while (r > 0) {
+			r = ::read(mPort, &dummy, sizeof(dummy)); // read flush
+		}
+	}
+
+	/** tries to read up to incomingLength bytes from filedescriptor
+	 *
+	 * return value
+	 *  [timeoutFlag, valid, parameters] = readPacket(...);
+	 *
+	 *  timeoutFlag indicates if a timeout has occurred
+	 *  valid       inidcates if parameters form a valid packet
+	 *  paremeters  is a vector with read bytes
+	 */
+	[[nodiscard]] auto readPacket(uint8_t incomingLength, Timeout timeout) const -> std::tuple<bool, bool, Parameter> {
 		auto startTime = now();
 
-		i_packet.resize(incommingLength);
-		uint8_t *data = i_packet.data();
+		Parameter rxBuf(incomingLength);
+		uint8_t *data = rxBuf.data();
 		size_t bytesRead = 0;
 
-		int headerReceived = 0;
+		bool timeoutFlag = false;
+
 		do {
-			ssize_t r = ::read(port, &i_packet[headerReceived], 1);
-			if (r < 0) {
-				continue;
-			}
-//			std::cout << int(i_packet[headerReceived]) << " ";
-			if (i_packet[headerReceived] == 0xff) {
-				if (headerReceived < 2) {
-					++headerReceived;
-				}
-			} else if (2 == headerReceived) {
-				bytesRead = 3;
-				break;
-			} else {
-				headerReceived = 0;
-			}
-		} while (timeout.count()==0 || now() - startTime < timeout);
-		do {
-			ssize_t r = ::read(port, data + bytesRead, incommingLength - bytesRead);
+			ssize_t r = ::read(mPort, data + bytesRead, incomingLength - bytesRead);
 			if (r > 0) {
 				bytesRead += r;
+				// check if first two bytes are 0xff
+				if (bytesRead >= 2) {
+					if (rxBuf[1] != 0xff) {
+						bytesRead = 0;
+					} else if (rxBuf[0] == 0xff) {
+						bytesRead = 0;
+					} else {
+						bytesRead -= 1;
+						memmove(rxBuf.data(), rxBuf.data()+1, bytesRead);
+					}
+				}
 			} else if (errno != EAGAIN) {
 				std::cout << "r: " << r << std::endl;
 				break;
 			}
-		} while (bytesRead < incommingLength && (timeout.count()==0 || now() - startTime < timeout));
+			timeoutFlag = (timeout.count() != 0) and (now() - startTime >= timeout);
+		} while (bytesRead < incomingLength && not timeoutFlag);
 
+		bool valid = bytesRead == incomingLength and validatePacket(rxBuf);
 
-		if (bytesRead != incommingLength) {
-			uint8_t dummy;
-			ssize_t r = 1;
-			while (r > 0) {
-				r = ::read(port, &dummy, sizeof(dummy)); // read flush
-			}
-		}
-		i_packet.resize(bytesRead);
-		return bytesRead == incommingLength;
+		return std::make_tuple(timeoutFlag, valid, std::move(rxBuf));
 	}
 
-	bool validatePacket(parameter const& i_packet, uint8_t motorID) {
-		bool success = 0xff == i_packet[0];
-		success &= 0xff == i_packet[1];
-		success &= motorID == i_packet[2];
-		success &= i_packet.size() - 4 == i_packet[3];
+	bool validatePacket(Parameter const& rxBuf) const {
+		bool success = 0xff == rxBuf[0];
+		success &= 0xff == rxBuf[1];
+		success &= 0xff != rxBuf[2];
+		success &= rxBuf.size() - 4 == rxBuf[3];
 		uint8_t checkSum = 0;
-		for (uint i(2); i < i_packet.size(); ++i) {
-			checkSum += i_packet[i];
+		for (uint i(2); i < rxBuf.size(); ++i) {
+			checkSum += rxBuf[i];
 		}
 		success &= 0xff == checkSum;
 		return success;
 	}
 
-	parameter getRxValuesFromPacket(parameter const& i_packet, uint8_t motorID) {
-		parameter returnParams;
-		bool success = validatePacket(i_packet, motorID);
-
-		if (success) {
-			returnParams.insert(returnParams.end(), i_packet.begin()+5, i_packet.end()-1);
-		} else {
-			std::cout << TERM_RED "received malformed packet" << std::endl;
-			for (auto byte : i_packet) {
-				printf("0x%X ", byte);
-			}
-			std::cout << TERM_RESET "\n";
+/*	void dumpBuffer(Parameter const& rxBuf) const {
+		std::cout << TERM_RED "received malformed packet" << std::endl;
+		for (auto byte : i_packet) {
+			printf("0x%X ", byte);
 		}
-
-		return returnParams;
-	}
+		std::cout << TERM_RESET "\n";
+	}*/
 };
 
 USB2Dynamixel::USB2Dynamixel(int baudrate, std::vector<std::string> const& deviceNames)
-	: m_pimpl(new Pimpl(deviceNames, baudrate))
+	: m_pimpl(std::make_unique<Pimpl>(deviceNames, baudrate))
 {}
 
 USB2Dynamixel::~USB2Dynamixel() {
 }
 
-void USB2Dynamixel::setBaudrate(uint newBaudrate)
-{
-	std::lock_guard<std::mutex> lock(mMutex);
-	m_pimpl->port.setBaudrate(newBaudrate);
-}
-
-bool USB2Dynamixel::ping(dynamixel::motorID motor, timeout_t timeout)
-{
-	std::lock_guard<std::mutex> lock(mMutex);
-	std::vector<uint8_t> txBuf{
-		0xff, 0xff, motor, 2, uint8_t(dynamixel::Instruction::PING),
-	};
-	txBuf.push_back(calculateChecksum(txBuf));
-
-	std::vector<uint8_t> rxBuf(6, 0);
-	m_pimpl->writePacket(txBuf);
-	m_pimpl->readPacket(rxBuf, rxBuf.size(), timeout);
-	return m_pimpl->validatePacket(rxBuf, motor);
-}
-
-bool USB2Dynamixel::read(dynamixel::motorID motor, int baseRegister, uint8_t length, timeout_t timeout, callback cb)
-{
-	std::lock_guard<std::mutex> lock(mMutex);
-	std::vector<uint8_t> txBuf{
-		0xff, 0xff, motor, 4, uint8_t(dynamixel::Instruction::READ), uint8_t(baseRegister), length
-	};
-	txBuf.push_back(calculateChecksum(txBuf));
-	std::vector<uint8_t> rxBuf(6+length, 0);
-
-	m_pimpl->writePacket(txBuf);
-	m_pimpl->readPacket(rxBuf, rxBuf.size(), timeout);
-	parameter recParams = m_pimpl->getRxValuesFromPacket(rxBuf, motor);
-	if (cb) {
-		cb(motor, recParams.data(), recParams.size());
+namespace {
+	[[nodiscard]]
+	uint8_t calculateChecksum(Parameter const& packet) {
+		uint32_t checkSum = 0;
+		for (size_t i(2); i < packet.size(); ++i) {
+			checkSum += packet[i];
+		}
+		return ~checkSum;
 	}
-	return recParams.size();
-}
 
-bool USB2Dynamixel::write(dynamixel::motorID motor, int baseRegister, dynamixel::parameter const& writeBuffer)
-{
-	std::lock_guard<std::mutex> lock(mMutex);
-	if (not writeBuffer.empty()) {
-		std::vector<uint8_t> txBuf{
-			0xff, 0xff, motor,
-			uint8_t(3 + writeBuffer.size()), uint8_t(dynamixel::Instruction::WRITE), uint8_t(baseRegister),
+	[[nodiscard]]
+	auto constructBuffer(MotorID motorID, Instruction instr, std::optional<uint8_t> baseRegister = {}, std::vector<uint8_t> data = {}) {
+		uint8_t length = 2 + data.size() + (baseRegister.has_value()?1:0);
+
+		std::vector<uint8_t> txBuf {
+			0xff, 0xff, motorID, length, uint8_t(instr)
 		};
-		txBuf.insert(txBuf.end(), writeBuffer.begin(), writeBuffer.end());
+		if (baseRegister) {
+			txBuf.push_back(baseRegister.value());
+		}
+		txBuf.insert(txBuf.end(), data.begin(), data.end());
+
 		txBuf.push_back(calculateChecksum(txBuf));
-		m_pimpl->writePacket(txBuf);
-		return true;
+		return txBuf;
 	}
-	return false;
 }
 
-bool USB2Dynamixel::reset(dynamixel::motorID motor)
+bool USB2Dynamixel::ping(MotorID motor, Timeout timeout)
 {
-	std::lock_guard<std::mutex> lock(mMutex);
-	std::vector<uint8_t> txBuf{
-		0xff, 0xff, motor,
-		2, uint8_t(dynamixel::Instruction::RESET),
-	};
-	txBuf.push_back(calculateChecksum(txBuf));
+	auto g = std::lock_guard(m_pimpl->mMutex);
+	auto txBuf = constructBuffer(motor, Instruction::PING);
+
 	m_pimpl->writePacket(txBuf);
-	return true;
+	auto [timeoutFlag, valid, rxBuf] = m_pimpl->readPacket(6, timeout);
+	return not timeoutFlag and valid;
 }
 
-bool USB2Dynamixel::sync_write(std::map<dynamixel::motorID, dynamixel::parameter> const& motorParams, int baseRegister)
-{
-	std::lock_guard<std::mutex> lock(mMutex);
-	if (motorParams.size() > 0)
-	{
-		const uint8_t len = motorParams.begin()->second.size();
-		bool okay = true;
-		for (std::pair<dynamixel::motorID, dynamixel::parameter> const& param : motorParams)
-		{
-			okay &= param.second.size() == len;
-		}
+auto USB2Dynamixel::read(MotorID motor, int baseRegister, uint8_t length, Timeout timeout) -> std::tuple<bool, bool, std::vector<uint8_t>> {
+	auto g = std::lock_guard(m_pimpl->mMutex);
+	auto txBuf = constructBuffer(motor, Instruction::READ, uint8_t(baseRegister), {length});
 
-		if (len > 0 && okay)
-		{
-			const uint8_t packetLength = 6 + (len + 1) * motorParams.size();
-			std::vector<uint8_t> txBuf;
-			txBuf.reserve(packetLength);
-			txBuf.push_back(0xff);
-			txBuf.push_back(0xff);
-			txBuf.push_back(dynamixel::broadcastID);
-			txBuf.push_back(4 + (len + 1) * motorParams.size());
-			txBuf.push_back(uint8_t(dynamixel::Instruction::SYNC_WRITE));
-			txBuf.push_back(uint8_t(baseRegister));
-			txBuf.push_back(len);
+	m_pimpl->writePacket(txBuf);
+	auto [timeoutFlag, valid, rxBuf] = m_pimpl->readPacket(6+length, timeout);
 
-			for (auto const& param : motorParams) {
-				txBuf.push_back(param.first);
-				txBuf.insert(txBuf.end(), param.second.begin(), param.second.end());
-			}
-
-			txBuf.push_back(calculateChecksum(txBuf));
-			m_pimpl->writePacket(txBuf);
-			return true;
-		}
+	auto rxBuf2 = std::vector<uint8_t>{};
+	if (valid) {
+		rxBuf2 = {std::next(begin(rxBuf), 5), std::next(begin(rxBuf), rxBuf.size()-1)};
 	}
-	return false;
+	return std::make_tuple(timeoutFlag, valid, rxBuf2);
 }
 
+void USB2Dynamixel::write(MotorID motor, int baseRegister, Parameter const& writeBuffer) {
+	auto g = std::lock_guard(m_pimpl->mMutex);
+	auto txBuf = constructBuffer(motor, Instruction::WRITE, uint8_t(baseRegister), writeBuffer);
+	m_pimpl->writePacket(txBuf);
+}
 
-uint8_t USB2Dynamixel::calculateChecksum(dynamixel::parameter const& packet) const
-{
-	int checkSum = 0;
-	for (uint i(2); i < packet.size(); ++i) {
-		checkSum += packet[i];
+void USB2Dynamixel::reset(MotorID motor) {
+	auto g = std::lock_guard(m_pimpl->mMutex);
+	auto txBuf = constructBuffer(motor, Instruction::RESET);
+	m_pimpl->writePacket(txBuf);
+}
+
+void USB2Dynamixel::sync_write(std::map<MotorID, Parameter> const& motorParams, int baseRegister) {
+	auto g = std::lock_guard(m_pimpl->mMutex);
+
+	if (motorParams.empty() > 0) {
+		throw std::runtime_error("sync_write: motorParams can't be empty");
 	}
-	return ~checkSum;
+
+	const uint8_t len = motorParams.begin()->second.size();
+	bool const okay = std::all_of(begin(motorParams), end(motorParams), [&](auto param) {
+		return param.second.size() == len;
+	});
+
+	if (len <= 0 or not okay) {
+		throw std::runtime_error("sync_write: data is not consistent");
+	}
+
+	std::vector<uint8_t> data;
+	data.push_back(len);
+	for (auto const& [id, params] : motorParams) {
+		data.push_back(id);
+		data.insert(data.end(), params.begin(), params.end());
+	}
+
+	auto txBuf = constructBuffer(BroadcastID, Instruction::SYNC_WRITE, uint8_t(baseRegister), data);
+	m_pimpl->writePacket(txBuf);
 }
+
 }
--- src/usb2dynamixel/USB2Dynamixel.h
+++ src/usb2dynamixel/USB2Dynamixel.h
@@ -1,48 +1,34 @@
 #pragma once
 
-#include <string>
-#include <map>
-#include <mutex>
-#include <memory>
-#include <functional>
-
 #include "dynamixel.h"
 
+#include <functional>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
 
-namespace dynamixel
-{
-
-using MotorID = uint8_t;
-constexpr MotorID MotorIDInvalid = 0xFF;
-constexpr MotorID MotorIDAll     = 0xFE;
+namespace dynamixel {
 
 struct USB2Dynamixel {
-	using timeout_t = std::chrono::microseconds;
-	typedef std::function<void(dynamixel::motorID motor, const uint8_t* receiveBuffer, uint8_t rxPayloadLen)> callback;
+	using Timeout = std::chrono::microseconds;
 
 	USB2Dynamixel(int baudrate, std::vector<std::string> const& deviceNames);
 	~USB2Dynamixel();
 
-	void setBaudrate(uint newBaudrate);
+	[[nodiscard]] bool ping(MotorID motor, Timeout timeout);
 
-	bool ping(dynamixel::motorID motor, timeout_t timeout);
+	[[nodiscard]] auto read(MotorID motor, int baseRegister, uint8_t length, Timeout timeout) -> std::tuple<bool, bool, std::vector<uint8_t>>;
 
-	bool read(dynamixel::motorID motor, int baseRegister, uint8_t length, timeout_t timeout, callback cb);
+	void write(MotorID motor, int baseRegister, Parameter const& writeBuffer);
 
-	bool write(dynamixel::motorID motor, int baseRegister, dynamixel::parameter const& writeBuffer);
+	void reset(MotorID motor);
 
-	// write reg is not implemented since it's not necessary see sync_write
-	// action is not implemented (see above)
-
-	bool reset(dynamixel::motorID motor);
-
-	bool sync_write(std::map<dynamixel::motorID, dynamixel::parameter> const& motorParams, int baseRegister);
+	void sync_write(std::map<MotorID, Parameter> const& motorParams, int baseRegister);
 
 private:
-	class Pimpl;
+	struct Pimpl;
 	std::unique_ptr<Pimpl> m_pimpl;
-	uint8_t calculateChecksum(dynamixel::parameter const& packet) const;
-	mutable std::mutex mMutex;
 };
 
 }
